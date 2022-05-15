d77189761d0c906c7efa6a26f2da9ce2c7252d27
micaelsgarcez <99693843+micaelsgarcez@users.noreply.github.com>
2022-05-15 10:43:09 -0300

    Added hero section and header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#04101D',
+        secondary: '#D84040'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
